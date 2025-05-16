8c3ad920dc7de6cf60fb3970a12e1f10e1c11c37
rammy427 <grex4274@gmail.com>
2025-05-15 21:40:21 -0400

    Indentation change.

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -18,13 +18,13 @@ function Home() {
 
     return (
         <>
-            <h1>{user.FirstName} {user.LastName}</h1>
-            <img src="/src/assets/logo.jpeg" className="w-25 mt-3 mb-3 rounded-circle align-self-center"></img>
-            <h2>{user.Role}</h2>
-            <p className="col-8 mx-auto">{user.Description}</p>
+        <h1>{user.FirstName} {user.LastName}</h1>
+        <img src="/src/assets/logo.jpeg" className="w-25 mt-3 mb-3 rounded-circle align-self-center"></img>
+        <h2>{user.Role}</h2>
+        <p className="col-8 mx-auto">{user.Description}</p>
 
-            {/* Get skills from the component. */}
-            <Skills />
+        {/* Get skills from the component. */}
+        <Skills />
         </>
     );
 }
