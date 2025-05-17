751dbe2d877f9cb48e8cf3cd006f26694d42929f
rammy427 <grex4274@gmail.com>
2025-05-16 21:45:12 -0400

    Fixed removeItem function call bug.

--- frontend/src/components/DeleteModal.jsx
+++ frontend/src/components/DeleteModal.jsx
@@ -27,7 +27,7 @@ function DeleteModal({type, id})
         </button>
 
         {/* Modal */}
-        <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div className="modal-dialog">
                 <div className="modal-content">
                 <div className="modal-header">
@@ -39,7 +39,7 @@ function DeleteModal({type, id})
                 </div>
                 <div className="modal-footer">
                     <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                    <button type="button" className="btn btn-danger" onClick={removeItem()}>Delete</button>
+                    <button type="button" className="btn btn-danger" onClick={removeItem}>Delete</button>
                 </div>
                 </div>
             </div>
