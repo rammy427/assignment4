bd6acdbeb757a7c36e2342bcd2395debe7fae629
rammy427 <grex4274@gmail.com>
2025-05-17 00:05:27 -0400

    Verified skill routes with JWT.

--- backend/src/routes/skillRoutes.js
+++ backend/src/routes/skillRoutes.js
@@ -2,12 +2,13 @@ const express = require("express");
 const skillController = require("../controllers/skillController");
 const {validateId, validateUserId} = require("../validations/idValidation");
 const {validateSkill} = require("../validations/skillValidation");
+const verifyJWT = require("../middleware/jwt");
 
 const router = express.Router();
 
 router.get("/:userId", validateUserId, skillController.getSkillsByUser);
-router.post("/:userId", validateUserId, validateSkill, skillController.addSkillToUser);
-router.put("/:userId/:id", validateUserId, validateId, validateSkill, skillController.updateSkill);
-router.delete("/:userId/:id", validateUserId, validateId, skillController.deleteSkill);
+router.post("/:userId", verifyJWT, validateUserId, validateSkill, skillController.addSkillToUser);
+router.put("/:userId/:id", verifyJWT, validateUserId, validateId, validateSkill, skillController.updateSkill);
+router.delete("/:userId/:id", verifyJWT, validateUserId, validateId, skillController.deleteSkill);
 
 module.exports = router;
