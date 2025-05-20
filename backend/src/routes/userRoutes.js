046f67341ec6d6a97e1adaf3782df4090c96de59
rammy427 <grex4274@gmail.com>
2025-05-19 20:57:04 -0400

    Removed unused route.

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const userController = require("../controllers/userController");
 const {validateId} = require("../validations/idValidation");
-const {validateUser, validateUserEdit} = require("../validations/userValidation");
+const { validateUserEdit } = require("../validations/userValidation");
 const verifyJWT = require("../middleware/jwt");
 const userRemoved = require("../middleware/userRemoved");
 
 const router = express.Router();
 
 router.get("/:id", validateId, userController.getUserById);
-router.post("/", userRemoved, validateUser, userController.createUser);
 router.put("/:id", userRemoved, verifyJWT, validateId, validateUserEdit, userController.updateUser);
 router.delete("/:id", userRemoved, verifyJWT, validateId, userController.deleteUser);
 
